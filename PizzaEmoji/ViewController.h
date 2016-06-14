1776362b824de000dbdfd55fecf36df20df9247f
Paul Kitchener <p.a.kitchener@gmail.com>
2016-06-13 20:13:42 -0500

    initial commit

---
+++ PizzaEmoji/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PizzaEmoji
+//
+//  Created by Paul Kitchener on 6/13/16.
+//  Copyright © 2016 Box Score Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PizzaEmoji/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PizzaEmoji
+//
+//  Created by Paul Kitchener on 6/13/16.
+//  Copyright © 2016 Box Score Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
