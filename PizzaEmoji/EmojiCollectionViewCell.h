65393563c3c815adcca806b3ad65b948cf808a02
Paul Kitchener <p.a.kitchener@gmail.com>
2016-06-14 09:15:46 -0500

    Updated Emoji Collection View Cell.

---
+++ PizzaEmoji/EmojiCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.h
+//  PizzaEmoji
+//
+//  Created by Paul Kitchener on 6/13/16.
+//  Copyright © 2016 Box Score Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmojiCollectionViewCell : UICollectionViewCell
+
+@property (strong, nonatomic) IBOutlet UIImageView *emojiImageView;
+
+@end
---
+++ PizzaEmoji/EmojiCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  EmojiCollectionViewCell.m
+//  PizzaEmoji
+//
+//  Created by Paul Kitchener on 6/13/16.
+//  Copyright © 2016 Box Score Games. All rights reserved.
+//
+
+#import "EmojiCollectionViewCell.h"
+
+@implementation EmojiCollectionViewCell
+
+@end
